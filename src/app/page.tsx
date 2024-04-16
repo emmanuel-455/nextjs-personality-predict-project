e791877606939b1f554eb7425b8d04226293b0a5
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-04-17 00:44:39 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,36 @@
-
+"use client";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
 
 export default function Home() {
+  const [value, setValue] = useState("");
+  const { push } = useRouter();
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    push(`/prediction/${value}`);
+  };
   return (
-    <div>
-      <div>
-        <h1>Enter your name</h1>
-        <input type="text" placeholder="Enter" />
-        <button type="submit">Predict Data</button>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="p-4 shadow-md bg-white rounded-md">
+        <h1 className="text-2xl font-semibold mb-4 text-black">
+          Enter Your Name
+        </h1>
+        <form onSubmit={handleSubmit} className="space-y-3">
+          <input
+            type="text"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded text-black"
+            placeholder="Type something..."
+          />
+          <button
+            type="submit"
+            className="w-full py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
